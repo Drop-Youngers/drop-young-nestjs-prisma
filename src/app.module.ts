b6e3a0b869a6ab21058630ec9a713fb18ad1c73e
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-10-05 11:05:44 +0200

    [Add] Added more configuratons in the projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { UserModule } from './user/user.module';
 import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [UsersModule, UserModule, MailModule],
+  imports: [UserModule, MailModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppExceptionFilter } from './filters/AppExceptionFilter';
+import { configDotenv } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
+  configDotenv()
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api/v1');
+  app.enableCors({ origin: "*" });
+  app.useGlobalFilters(new AppExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({}));
+
+  const config = new DocumentBuilder()
+    .setTitle('AIC API documentation')
+    .setDescription('The documentation for the AIC API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .addTag('auth')
+    .addTag('users')
+    .addTag('artists')
+    .addTag('streams')
+    .addTag("faqs")
+    .addTag("contacts")
+    .addTag("health")
+    .setContact("Jazzy Bruno" , "https://github.com/jazzybruno" , "jazzybruno45@gmail.com")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/v1/docs', app, document);
+
+
+  await app.listen(process.env.PORT);
+
 }
+
 bootstrap();
